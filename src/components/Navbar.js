b199d1b82c30bd0bae823808c8d4138a40178bd3
koko1313 <kaloyanvellichkov@gmail.com>
2022-11-04 18:13:36 +0200

    Fix the bug where home button is always active

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ export default function Navbar () {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="/">Начало</NavLink>
+                            <NavLink className="nav-link" to="/" end>Начало</NavLink>
                         </li>
                         
                         {jwtToken && 
